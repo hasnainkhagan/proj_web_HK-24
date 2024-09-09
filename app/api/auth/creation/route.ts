af9ce07f4d8800249240b2cb55aeacd246fb70a5
Hasnain Khan🏴 <official.thevm@gmail.com>
2024-09-09 05:38:33 +0500

    project done

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -30,5 +30,5 @@ export async function GET() {
     });
   }
 
-  return NextResponse.redirect("http://localhost:3000/guestbook");
+  return NextResponse.redirect("https://hasnainkhagan.vercel.app/guestbook");
 }
